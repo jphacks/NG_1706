1c3300619eb4c5a060b16bc8a3618c609a4e9a58
aratomo <aratomo@srmtlab.org>
2017-10-21 09:58:07 +0900

    first commit

---
+++ iPhoneSwift/translater/translaterTests/translaterTests.swift
@@ -0,0 +1,36 @@
+//
+//  translaterTests.swift
+//  translaterTests
+//
+//  Created by Tomoya Arakawa on 2017/10/21.
+//  Copyright © 2017年 Tomoya Arakawa. All rights reserved.
+//
+
+import XCTest
+@testable import translater
+
+class translaterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
